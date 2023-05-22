99b52dc6c51f534ebaa7e16e19aa026deaa18221
b-laporte <brt.laporte@gmail.com>
2023-05-22 15:48:07 +0200

    initial commit: code transfer from @asimojs/asimo 8d28f89

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// vite.config.js
+import { resolve } from 'path';
+import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [preact()],
+    test: {
+        environment: 'jsdom'
+    },
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/lml.js'),
+            name: 'lml',
+            fileName: 'lml',
+        },
+        rollupOptions: {
+
+        },
+    }
+})
+
